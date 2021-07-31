30a81a8fdce8075f17985bf19c0051e7abd112fb
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-07-31 16:34:29 +0300

    renamed TimeRecordsEntity to TimeRecordEntity

--- service/src/main/java/team/healthtech/service/mapper/TimeRecordMapper.java
+++ service/src/main/java/team/healthtech/service/mapper/TimeRecordMapper.java
@@ -1,7 +1,7 @@
 package team.healthtech.service.mapper;
 
 import org.mapstruct.Mapper;
-import team.healthtech.db.entity.TimeRecordsEntity;
+import team.healthtech.db.entity.TimeRecordEntity;
 import team.healthtech.service.model.TimeRecordDto;
 
 import java.util.List;
@@ -9,10 +9,10 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface TimeRecordMapper {
 
-    TimeRecordDto fromEntity(TimeRecordsEntity entity);
+    TimeRecordDto fromEntity(TimeRecordEntity entity);
 
-    TimeRecordsEntity toEntity(TimeRecordDto dto);
+    TimeRecordEntity toEntity(TimeRecordDto dto);
 
-    List<TimeRecordDto> fromEntities(Iterable<TimeRecordsEntity> entities);
+    List<TimeRecordDto> fromEntities(Iterable<TimeRecordEntity> entities);
 
 }
--- service/src/main/java/team/healthtech/service/model/DiseaseDto.java
+++ service/src/main/java/team/healthtech/service/model/DiseaseDto.java
@@ -1,14 +1,4 @@
 package team.healthtech.service.model;
 
-public class TimeRecordsDto {
-
-    private Integer id;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
+public class DiseaseDto {
 }
