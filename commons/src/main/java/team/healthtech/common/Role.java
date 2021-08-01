3e3a00deda1cb312047f9b8d929a7a91f9ed339b
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2021-08-01 13:31:54 +0300

    include commons

---
+++ commons/src/main/java/team/healthtech/common/Role.java
@@ -0,0 +1,34 @@
+package team.healthtech.common;
+
+import java.util.Optional;
+
+public enum Role {
+
+    ADMIN(1),
+    DOCTOR(2),
+    PATIENT(3);
+
+    private final int id;
+
+    Role(int id) {
+        this.id = id;
+    }
+
+    public static Optional<Role> of(Integer id) {
+        if (id == null) {
+            return Optional.empty();
+        }
+
+        for (var value: Role.values()) {
+            if (value.id == id) {
+                return Optional.of(value);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public int getId() {
+        return id;
+    }
+}
