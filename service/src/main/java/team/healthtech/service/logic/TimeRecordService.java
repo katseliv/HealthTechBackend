2bb1bfa2c497fa397cb4a470f357bec543b474e9
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2021-08-02 20:49:33 +0300

    TimeRecordServiceImpl finished, PatientDiseasesServiceImpl finished

--- service/src/main/java/team/healthtech/service/logic/TimeRecordService.java
+++ service/src/main/java/team/healthtech/service/logic/TimeRecordService.java
@@ -11,10 +11,6 @@ public interface TimeRecordService {
 
     void updateTimeRecord(TimeRecordDto timeRecordDto, int timeRecordsId);
 
-    // учитывая, что у доктора только одно расписание, будет ли это нормой?
-    // или у доктора может быть два расписания?
-    void deleteTimeRecordsByDoctorId(int doctorId);
-
     void deleteTimeRecord(int timeRecordId);
 
 }
