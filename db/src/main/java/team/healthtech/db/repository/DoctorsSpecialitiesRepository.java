d0603a9a28010e54129263008722c05dc75724a4
katseliv <katseliv@yandex.ru>
2021-08-04 14:03:50 +0300

    Finished DoctorSpecialitiesServiceImpl

--- db/src/main/java/team/healthtech/db/repository/DoctorsSpecialitiesRepository.java
+++ db/src/main/java/team/healthtech/db/repository/DoctorsSpecialitiesRepository.java
@@ -4,11 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import team.healthtech.db.entity.AppointmentEntity;
 import team.healthtech.db.entity.DoctorSpecialityId;
 import team.healthtech.db.entity.DoctorsSpecialitiesEntity;
 
+import java.util.List;
+
 @Repository
 public interface DoctorsSpecialitiesRepository extends CrudRepository<DoctorsSpecialitiesEntity, DoctorSpecialityId>, JpaRepository<DoctorsSpecialitiesEntity, DoctorSpecialityId>, JpaSpecificationExecutor<DoctorsSpecialitiesEntity> {
+
+    List<DoctorsSpecialitiesEntity> getDoctorsSpecialitiesEntitiesByDoctorId(Integer doctorId);
 
 }
--- rest/src/main/java/team/healthtech/rest/api/DoctorSpecialitiesController.java
+++ rest/src/main/java/team/healthtech/rest/api/DoctorSpecialitiesController.java
@@ -1,9 +1,11 @@
 package team.healthtech.rest.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.healthtech.service.logic.DoctorSpecialitiesService;
 import team.healthtech.service.model.DoctorSpecialitiesDto;
 
 import java.util.List;
@@ -12,9 +14,16 @@ import java.util.List;
 @RequestMapping("/doctors/{doctorId}/specialities")
 public class DoctorSpecialitiesController {
 
+    private final DoctorSpecialitiesService service;
+
+    @Autowired
+    public DoctorSpecialitiesController(DoctorSpecialitiesService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public List<DoctorSpecialitiesDto> getAllSpecialitiesFromDoctor(@PathVariable int doctorId) {
-        return null;
+        return service.getAllSpecialitiesByDoctorId(doctorId);
     }
 
 }
