b20e93613558475c9d34eb38044baa1130770352
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2021-08-08 03:13:58 +0300

    time record service logger implemented

--- service/src/main/java/team/healthtech/service/logic/impl/TimeRecordServiceImpl.java
+++ service/src/main/java/team/healthtech/service/logic/impl/TimeRecordServiceImpl.java
@@ -1,29 +1,34 @@
 package team.healthtech.service.logic.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import team.healthtech.db.entity.TimeRecordEntity;
 import team.healthtech.db.repository.TimeRecordsRepository;
 import team.healthtech.service.logic.TimeRecordService;
 import team.healthtech.service.mapper.TimeRecordMapper;
-import team.healthtech.service.model.AppointmentDto;
-import team.healthtech.service.model.DiseaseDto;
 import team.healthtech.service.model.TimeRecordDto;
+import team.healthtech.service.security.Profile;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 @Validated
 public class TimeRecordServiceImpl implements TimeRecordService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TimeRecordServiceImpl.class);
+    private final ObjectProvider<Profile> profileProvider;
     private final TimeRecordsRepository repository;
     private final TimeRecordMapper mapper;
 
-    public TimeRecordServiceImpl(TimeRecordsRepository repository, TimeRecordMapper mapper) {
+    public TimeRecordServiceImpl(
+        ObjectProvider<Profile> profileProvider,
+        TimeRecordsRepository repository,
+        TimeRecordMapper mapper
+    ) {
+        this.profileProvider = profileProvider;
         this.repository = repository;
         this.mapper = mapper;
     }
@@ -39,12 +44,14 @@ public class TimeRecordServiceImpl implements TimeRecordService {
 
     @Override
     public void updateTimeRecord(TimeRecordDto timeRecordDto, int timeRecordsId) {
+        logger.info("Time record with id {} update request by {}", timeRecordsId, profileProvider.getIfAvailable());
         mapper.merge(timeRecordDto, repository.findById(timeRecordsId).orElseThrow());
         repository.save(mapper.toEntity(timeRecordDto));
     }
 
     @Override
     public void deleteTimeRecord(int timeRecordId) {
+        logger.info("Time record with id {} delete request by {}", timeRecordId, profileProvider.getIfAvailable());
         repository.deleteById(timeRecordId);
     }
 
